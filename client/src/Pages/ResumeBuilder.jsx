c72307f384c7a5c0b441ea025cf5dba06bf50e19
rupesh0001-tech <rupeshcoding01@gmail.com>
2025-10-30 14:26:52 +0530

    auto-save

--- client/src/Pages/ResumeBuilder.jsx
+++ client/src/Pages/ResumeBuilder.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import dummyResumeData from './../assets/assets.js';
 const ResumeBuilder = () => {
+  const {resumeId} = useParams();
+
+  const [resumeData, setResumedata] = useState(
+    {
+    _id: '',
+    title: '',
+    personal_info: {},
+    professional_summary: "",
+    experience: [],
+    education: [],
+    project: [],
+    skills: [],
+    template: "classic",
+    accent_color: "#3B82F6",
+    public: false,
+}
+  )
+
+  const loadResume = () => {
+    let resume = dummyResumeData.find(resume => resume._id === resumeId);
+    return resume; 
+};
+console.log(resumeId)
+
   return (
     <div>ResumeBuilder</div>
   )
